3e930ec498d934b69974b2b531495543911ccba3
Gregg Fine <bbeez8500@gmail.com>
2020-07-14 15:33:41 -0400

    mobile styles

--- .next/static/runtime/webpack.js
+++ .next/static/runtime/webpack.js
@@ -113,7 +113,7 @@
 /******/
 /******/ 	var hotApplyOnUpdate = true;
 /******/ 	// eslint-disable-next-line no-unused-vars
-/******/ 	var hotCurrentHash = "ca9e83413758e471e7e8";
+/******/ 	var hotCurrentHash = "6cc480f4f9cca9b76b9a";
 /******/ 	var hotRequestTimeout = 10000;
 /******/ 	var hotCurrentModuleData = {};
 /******/ 	var hotCurrentChildModule;
